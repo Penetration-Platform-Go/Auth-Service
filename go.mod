0c23c2f4313796d8d81e887bae16866e29e7b3f9
ChrisJu <chris-ju@qq.com>
2020-02-10 16:54:01 +0800

    Cors

--- go.mod
+++ go.mod
@@ -3,7 +3,7 @@ module github.com/Penetration-Platform-Go/Auth-Service
 go 1.12
 
 require (
-	github.com/Penetration-Platform-Go/gRPC-Files v0.0.0-20200129091809-77c15b11ad8b
+	github.com/Penetration-Platform-Go/gRPC-Files v0.0.0-20200206094001-26eb7345d882
 	github.com/dgrijalva/jwt-go v3.2.0+incompatible
 	github.com/gin-gonic/gin v1.5.0
 	github.com/spf13/pflag v1.0.5
--- route/route.go
+++ route/route.go
@@ -2,12 +2,14 @@ package route
 
 import (
 	"github.com/Penetration-Platform-Go/Auth-Service/controller"
+	"github.com/Penetration-Platform-Go/Auth-Service/middleware"
 	"github.com/gin-gonic/gin"
 )
 
 // GetServer Return Gin Server
 func GetServer() *gin.Engine {
 	app := gin.Default()
+	app.Use(middleware.Cors())
 	app.POST("/auth", controller.LogInHandler)
 	return app
 }
